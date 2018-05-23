f0be90d64e98d9a776dd1d44475cc4744b9cd6ca
Jonjo McKay <jonjo@jonjomckay.com>
2018-05-23 22:38:20 +0100

    Added health check

---
+++ src/main/java/com/jonjomckay/flow/flowbot/health/HealthController.java
@@ -0,0 +1,14 @@
+package com.jonjomckay.flow.flowbot.health;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/health")
+public class HealthController {
+    @GET
+    public Response health() {
+        return Response.ok()
+                .build();
+    }
+}
